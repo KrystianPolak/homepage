27012ca9526e84ea749821e41cc21b11bdc8b879
Krystian Polak <kry.polak@o2.pl>
2022-01-30 20:15:32 +0100

    new object and functions in js

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-const first_name = "Krystian";
+const firstName = "Krystian";
 const age = 20;
-console.log(
-  `Witaj na mojej stronie nazywam się ${first_name} i mam ${age} lat`
-);
+const road = "country road";
+
+const volvo = {
+  year: 2005,
+  driving: (road) => {                                  // arrow function 
+    console.log(`volvo is driving on the ${road} `)
+  },
+  horsePower: 136,
+  color: "gray",
+  engine: 2.0,
+}
+
 
 const heading = document.querySelector('.main-header--js');
 console.log(heading);
 
+function hello(firstName, age) {                                                  
+  console.log(`Cześć nazwyam się ${firstName} i mam ${age} lat `);
+}
+
+hello(firstName, age);
+volvo.driving(road);
+console.log(volvo['year']);
+
+const myProperty = 'color'; 
+
+
+const showMeProperty = (myProperty) => {
+  console.log (volvo[myProperty]);
+}
+
+showMeProperty('horsePower');
